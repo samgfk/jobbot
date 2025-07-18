3f23bcb250a0436366ee68b5db476a9f2029a481
Jesse "James" Daniel Torres <jessetorres386@gmail.com>
2025-07-18 16:28:28 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -57,7 +57,7 @@ def get_jobs():
         scraped_jobs = scrape_remoteok()
 
         for job in scraped_jobs:
-            combined_text = f"{job['title']} {job['company']} {job['link']}".lower()
+            combined_text = f"{job['title']} {job['company']} {job['url']}".lower()
             if any(kw in combined_text for kw in KEYWORDS) and location_allowed(combined_text):
                 all_jobs.append(job)
                 if len(all_jobs) >= MAX_RESULTS:
